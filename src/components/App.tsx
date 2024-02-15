dcba88481a9ec10f1385c3e10e8d75c8cd92f94d
Leo Gancedo <gancedoleonel@gmail.com>
2024-02-15 17:45:00 -0300

    fin de esbuild

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React, { useCallback, useState } from 'react';
 
 const App = (props: { message: string }) => {
+    const [count, setCount] = useState(0)
+
+    const increment = useCallback(() => {
+        setCount((count) => count + 1)
+    }, [count])
     return (
         <>
             <h2>{props.message}</h2>
             <h3>Count: {count}</h3>
-            <button>Increment</button>
+            <button onClick={increment}>Increment</button>
         </>
     )
 }
+
+export default App;
